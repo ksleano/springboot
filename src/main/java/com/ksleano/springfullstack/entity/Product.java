af4dd92f3cf6dfda5b499f6ca76ff7718b3158c0
ksl <ksleano@gmail.com>
2021-04-05 11:14:40 -0700

    create product entity

--- src/main/java/com/ksleano/springfullstack/entity/Product.java
+++ src/main/java/com/ksleano/springfullstack/entity/Product.java
@@ -1,4 +1,37 @@
 package com.ksleano.springfullstack.entity;
 
+import com.sun.org.apache.xpath.internal.operations.Bool;
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@Entity
+@Table(name="product")
+@Data
 public class Product {
+
+    private long id;
+
+    private String sku;
+
+    private String name;
+
+    private String description;
+
+    private double unitPrice;
+
+    private String imageUrl;
+
+    private boolean active;
+
+    private int unitsInStock;
+
+    private Date dateCreated;
+
+    private Date lastUpdated;
+
+    private long categoryId;
 }
