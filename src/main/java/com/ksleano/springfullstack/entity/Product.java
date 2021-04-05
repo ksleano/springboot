e27597f76ee961d179122ba558b53e23b824a332
ksl <ksleano@gmail.com>
2021-04-05 12:06:40 -0700

    add entity ProductCategory, map ManyToOne/OneToMany

--- src/main/java/com/ksleano/springfullstack/entity/Product.java
+++ src/main/java/com/ksleano/springfullstack/entity/Product.java
@@ -2,9 +2,10 @@ package com.ksleano.springfullstack.entity;
 
 import com.sun.org.apache.xpath.internal.operations.Bool;
 import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.Date;
 
@@ -12,26 +13,44 @@ import java.util.Date;
 @Table(name="product")
 @Data
 public class Product {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
     private long id;
 
+    @ManyToOne
+    @JoinColumn(name="category_id", nullable = false)
+    private ProductCategory category;
+
+    @Column(name="sku")
     private String sku;
 
+    @Column(name="name")
     private String name;
 
+    @Column(name="description")
     private String description;
 
+    @Column(name="unit_price")
     private double unitPrice;
 
+    @Column(name="image_url")
     private String imageUrl;
 
+    @Column(name="active")
     private boolean active;
 
+    @Column(name="units_in_stock")
     private int unitsInStock;
 
+    @Column(name="date_created")
+    @CreationTimestamp
     private Date dateCreated;
 
+    @Column(name="last_updated")
+    @UpdateTimestamp
     private Date lastUpdated;
 
+    @Column(name="category_id")
     private long categoryId;
 }
