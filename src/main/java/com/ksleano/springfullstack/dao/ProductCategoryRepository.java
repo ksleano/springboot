9496063164559086e037f6491a34bc93a0495cbf
ksl <ksleano@gmail.com>
2021-04-06 22:06:15 -0700

    repository

--- src/main/java/com/ksleano/springfullstack/dao/ProductCategoryRepository.java
+++ src/main/java/com/ksleano/springfullstack/dao/ProductCategoryRepository.java
@@ -1,4 +1,10 @@
 package com.ksleano.springfullstack.dao;
 
-public interface ProductCategoryRepository {
+import com.ksleano.springfullstack.entity.ProductCategory;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.stereotype.Repository;
+
+@RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
+public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
 }
--- src/main/java/com/ksleano/springfullstack/dao/ProductRepository.java
+++ src/main/java/com/ksleano/springfullstack/dao/ProductRepository.java
@@ -1,4 +1,9 @@
 package com.ksleano.springfullstack.dao;
 
-public interface ProductRepository {
+import com.ksleano.springfullstack.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.stereotype.Repository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
 }
