e20c62e0c0942f52c95ea2767a93f01cd3b40537
ksl <ksleano@gmail.com>
2021-04-08 11:16:14 -0700

    quick notes on RepositoryRestResource

--- src/main/java/com/ksleano/springfullstack/dao/ProductCategoryRepository.java
+++ src/main/java/com/ksleano/springfullstack/dao/ProductCategoryRepository.java
@@ -5,6 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
+// without the annotation, spring will configure the path to "productCategories"
+// If you want to specify the path, use the annotation @RepositoryRestResource
 @RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
 }
